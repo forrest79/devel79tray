ff650795decd5e7a99f9252c3f0149f4d3847fef
Forrest79 <jakub@trmota.cz>
2012-03-28 13:19:12 +0200

    - update generated project

--- Devel79Tray/Devel79Tray.cpp
+++ Devel79Tray/Devel79Tray.cpp
@@ -16,7 +16,7 @@
 // CDevel79TrayApp
 
 BEGIN_MESSAGE_MAP(CDevel79TrayApp, CWinApp)
-	ON_COMMAND(ID_APP_ABOUT, &CDevel79TrayApp::OnAppAbout)
+	//ON_COMMAND(ID_APP_ABOUT, &CDevel79TrayApp::OnAppAbout)
 END_MESSAGE_MAP()
 
 
@@ -41,29 +41,16 @@ BOOL CDevel79TrayApp::InitInstance()
 	CWinApp::InitInstance();
 
 	// Standard initialization
-	// If you are not using these features and wish to reduce the size
-	// of your final executable, you should remove from the following
-	// the specific initialization routines you do not need
-	// Change the registry key under which our settings are stored
-	// TODO: You should modify this string to be something appropriate
-	// such as the name of your company or organization
-	SetRegistryKey(_T("Local AppWizard-Generated Applications"));
 
 	// To create the main window, this code creates a new frame window
 	// object and then sets it as the application's main window object
 	CMainFrame* pFrame = new CMainFrame;
-	if (!pFrame)
+	if (!pFrame) {
 		return FALSE;
+	}
 	m_pMainWnd = pFrame;
 	// create and load the frame with its resources
-	pFrame->LoadFrame(IDR_MAINFRAME,
-		WS_OVERLAPPEDWINDOW | FWS_ADDTOTITLE, NULL,
-		NULL);
-
-
-
-
-
+	pFrame->LoadFrame(IDR_MAINFRAME, WS_OVERLAPPEDWINDOW | FWS_ADDTOTITLE, NULL, NULL);
 
 	// The one and only window has been initialized, so show and update it
 	pFrame->ShowWindow(SW_SHOW);
@@ -73,48 +60,12 @@ BOOL CDevel79TrayApp::InitInstance()
 	return TRUE;
 }
 
-
-// CDevel79TrayApp message handlers
-
-
-
-
-// CAboutDlg dialog used for App About
-
-class CAboutDlg : public CDialog
-{
-public:
-	CAboutDlg();
-
-// Dialog Data
-	enum { IDD = IDD_ABOUTBOX };
-
-protected:
-	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
-
-// Implementation
-protected:
-	DECLARE_MESSAGE_MAP()
-};
-
-CAboutDlg::CAboutDlg() : CDialog(CAboutDlg::IDD)
-{
-}
-
-void CAboutDlg::DoDataExchange(CDataExchange* pDX)
-{
-	CDialog::DoDataExchange(pDX);
-}
-
-BEGIN_MESSAGE_MAP(CAboutDlg, CDialog)
-END_MESSAGE_MAP()
-
 // App command to run the dialog
-void CDevel79TrayApp::OnAppAbout()
-{
-	CAboutDlg aboutDlg;
-	aboutDlg.DoModal();
-}
+//void CDevel79TrayApp::OnAppAbout()
+//{
+//	CAboutDlg aboutDlg;
+//	aboutDlg.DoModal();
+//}
 
 // CDevel79TrayApp message handlers
 
--- Devel79Tray/Resource.h
+++ Devel79Tray/Resource.h
@@ -2,17 +2,16 @@
 // Microsoft Visual C++ generated include file.
 // Used by Devel79Tray.rc
 //
-#define IDD_ABOUTBOX				100
-#define IDR_MAINFRAME				128
-#define IDR_Devel79TrayTYPE				130
+#define IDR_MAINFRAME                   128
+#define IDR_Devel79TrayTYPE             130
 
 // Next default values for new objects
-//
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE	310
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		310
-#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_RESOURCE_VALUE        310
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           310
 #endif
 #endif
